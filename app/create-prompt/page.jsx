b3b5379f297ce9dbb02ce7d4670cc41fcb0314b2
Sahil Upadhyay <sahil.upadhyay@acquia.com>
2023-05-19 16:51:53 +0530

    creating and fetching prompt data

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,18 +2,41 @@
 
 import { useState } from 'react';
 import { useSession } from "next-auth/react";
-import { useRouter} from "next/navigation";
 import Form from '@components/Form'
+import { useRouter } from "next/navigation";
 
 function CreatePrompt(props) {
+  const router = useRouter();
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
     tag: ''
-  })
+  });
+
+  const {data: session} = useSession();
 
   const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      });
+
+      if(response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setSubmitting(false);
+    }
   }
   return (
     <Form
---
+++ components/Promptcard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PromptCard({tag, post, handleTagClick}) {
+  return (
+    <div>PromptCard</div>
+  );
+}
+
+export default PromptCard;
